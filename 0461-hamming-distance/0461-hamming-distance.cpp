c63d30062e3bb0ce464c10f32e1397844259a2fc
Kumar-Prince-Singh <152706431+Kumar-Prince-Singh@users.noreply.github.com>
2026-07-12 11:03:32 +0530

    Time: 0 ms (100%), Space: 7.9 MB (68.59%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        return __builtin_popcount(x ^ y);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
