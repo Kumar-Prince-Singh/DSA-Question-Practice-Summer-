94b1c678c3cf0fbfc98c090c4c06d549989840dc
Kumar-Prince-Singh <152706431+Kumar-Prince-Singh@users.noreply.github.com>
2026-07-14 14:13:39 +0530

    Time: 0 ms (100%), Space: 11.6 MB (93.68%) - LeetHub - Time: O(, Space: O(?)

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+
+        for (int i = 0; i < strs[0].size(); i++) {
+            char ch = strs[0][i];
+
+            for (int j = 1; j < strs.size(); j++) {
+                if (i >= strs[j].size() || strs[j][i] != ch) {
+                    return ans;
+                }
+            }
+
+            ans += ch;
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
