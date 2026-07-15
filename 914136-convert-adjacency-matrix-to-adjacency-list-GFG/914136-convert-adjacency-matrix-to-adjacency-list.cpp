4cc1fefb40d2517a8d3962dc155cf001e08f68fe
Kumar-Prince-Singh <152706431+Kumar-Prince-Singh@users.noreply.github.com>
2026-07-15 16:12:16 +0530

    Added solution - Time: O(?), Space: O(?)

---
+++ 914136-convert-adjacency-matrix-to-adjacency-list-GFG/914136-convert-adjacency-matrix-to-adjacency-list.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> matToAdj(vector<vector<int>>& mat) {
+
+        int n = mat.size();
+        vector<vector<int>> adj(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1) {
+                    adj[i].push_back(j);
+                }
+            }
+        }
+
+        return adj;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
