4340415b838133b26ea06581d96f5136e25b6226
Kumar-Prince-Singh <152706431+Kumar-Prince-Singh@users.noreply.github.com>
2026-07-16 15:26:39 +0530

    Added solution - Time: O(V + E), Space: O(V)

---
+++ 700203-dfs-of-graph-GFG/700203-dfs-of-graph.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    void DFS(int node, vector<vector<int>>& adj, vector<int>& ans, vector<bool>& visited) {
+        visited[node] = true;
+        ans.push_back(node);
+
+        for (int neighbour : adj[node]) {
+            if (!visited[neighbour]) {
+                DFS(neighbour, adj, ans, visited);
+            }
+        }
+    }
+
+    vector<int> dfs(vector<vector<int>>& adj) {
+        int V = adj.size();              
+        vector<int> ans;
+        vector<bool> visited(V, false);
+
+        DFS(0, adj, ans, visited);       
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
