67866d92683194a3eb8508edbfbdf71ca20439e9
Kumar-Prince-Singh <152706431+Kumar-Prince-Singh@users.noreply.github.com>
2026-09-20 22:51:15 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.46%) - LeetHub - Time: O(n * n!), Space: O(n)

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        backtrack(nums, 0, result);
+        return result;
+    }
+
+private:
+    void backtrack(vector<int>& nums, int start, vector<vector<int>>& result) {
+        if (start == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+
+        for (int i = start; i < nums.size(); ++i) {
+            swap(nums[start], nums[i]);
+            backtrack(nums, start + 1, result);
+            swap(nums[start], nums[i]);
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
